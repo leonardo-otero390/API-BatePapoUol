4d682886309f40037d2909e73998af63f26327f8
Leonardo Otero <leonardo.otero390@gmail.com>
2022-01-28 17:00:00 -0400

    feat: remove inactives

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import Joi from 'joi';
 import connection from '../database/connection.js';
 
@@ -25,5 +26,42 @@ export async function insert(req, res) {
     console.log(error);
     connection.mongoClient.close();
     return res.sendStatus(500);
+  }
+}
+function creatLeaveRoomMessage(from) {
+  return {
+    from,
+    to: 'Todos',
+    text: 'sai da sala...',
+    type: 'status',
+    time: dayjs().format('HH:mm:ss'),
+  };
+}
+
+export async function autoRemove() {
+  const SECONDS = 1000;
+  const lastValidStatus = Date.now() - 10 * SECONDS;
+  try {
+    await connection.mongoClient.connect();
+    const afkParticipants = await connection.db
+      .collection('participants')
+      .find(
+        { lastStatus: { $lt: lastValidStatus } },
+        { projection: { _id: 1, name: 1 } }
+      )
+      .toArray();
+    if (!afkParticipants.length) return;
+    const akfParticipantsIds = afkParticipants.map(({ _id }) => _id);
+    await connection.db
+      .collection('participants')
+      .deleteMany({ _id: { $in: akfParticipantsIds } });
+    const messages = afkParticipants.map(({ name }) =>
+      creatLeaveRoomMessage(name)
+    );
+    await connection.db.collection('messages').insertMany(messages);
+    connection.mongoClient.close();
+  } catch (error) {
+    console.log(error);
+    connection.mongoClient.close();
   }
 }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@
 
 import './setup.js';
 import app from './app.js';
+import { autoRemove } from './controllers/statusController.js';
+
+setInterval(autoRemove, 15000);
 
 app.listen(5000, () => {
   console.log(`Server is listening on port 5000.
