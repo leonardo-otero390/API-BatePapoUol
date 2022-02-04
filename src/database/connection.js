fe918289a8b4dbb2b4e2038664ef2b675571cf2f
Liaess <yann.melo@gmail.com>
2022-02-04 11:44:27 -0300

    Preparando revisão de código

--- src/database/connection.js
+++
@@ -1,9 +0,0 @@
-import '../setup.js';
-import { MongoClient } from 'mongodb';
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-const db = mongoClient.db(process.env.DB_NAME);
-
-const connection = { mongoClient, db };
-
-export default connection;
